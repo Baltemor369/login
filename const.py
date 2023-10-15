8e8fbff1682af9ad756e722895dcea4c930f93ab
Baltemor369 <118623957+Baltemor369@users.noreply.github.com>
2023-10-15 14:25:15 +0200

    add default param for tkinter widgets & colors

--- const.py
+++ const.py
@@ -1,8 +1,27 @@
-
 DB_USER = "locals/user_database.db"
 DB_LOG = "locals/log_database.db"
 
+BG = "#333333"
+FG = "#DDDDDD"
+
 LABEL = {
-    "fg":"",
-    "bg":"",
+    "fg":FG,
+    "bg":BG,
+}
+BUTTON7 = {
+    "width": 7,
+    "bg": "#0000FF",
+    "fg": FG
+}
+BUTTON10 = {
+    "width": 10,
+    "bg": "#0000FF",
+    "fg": FG
+}
+ENTRY = {
+    "bg": "#555555",
+    "fg": FG
+}
+FRAME = {
+    "bg": BG
 }
